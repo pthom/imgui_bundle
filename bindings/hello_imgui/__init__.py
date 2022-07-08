bcf928bbaee175bdaf023b142b9b6457eccf897b
Pascal Thomet <pthomet@gmail.com>
2022-07-08 20:57:10 +0200

    Review __init__.py imports

--- bindings/hello_imgui/__init__.py
+++ bindings/hello_imgui/__init__.py
@@ -1,9 +1,2 @@
-import os
-import sys
-
-this_dir = os.path.dirname(__file__)
-sys.path = [this_dir] + sys.path
-
-from _hello_imgui import imgui
-from _hello_imgui import hello_imgui
-#from _hello_imgui import *
+from hello_imgui._hello_imgui import imgui
+from hello_imgui._hello_imgui import hello_imgui
