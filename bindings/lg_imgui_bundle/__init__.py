08a7ca3477347a1cf1ceb81d6850272347c87b38
Pascal Thomet <pthomet@gmail.com>
2022-09-13 19:59:03 +0200

    Add icons_fontawesome

--- bindings/lg_imgui_bundle/__init__.py
+++ bindings/lg_imgui_bundle/__init__.py
@@ -1,8 +1,8 @@
 from lg_imgui_bundle._lg_imgui_bundle import imgui as imgui
 from lg_imgui_bundle._lg_imgui_bundle import hello_imgui as hello_imgui
 from lg_imgui_bundle._lg_imgui_bundle import implot as implot
+from lg_imgui_bundle import icons_fontawesome
 from lg_imgui_bundle._lg_imgui_bundle import __version__
-
 
 import os
 THIS_DIR = os.path.dirname(__file__)
