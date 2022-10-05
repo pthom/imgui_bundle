a76f060e6b0b4702e47f1940cfb8193948a1a98f
Pascal Thomet <pthomet@gmail.com>
2022-10-05 08:44:32 +0200

    regenerate

--- autogenerate_imgui_bundle/autogenerate_imgui_bundle.py
+++ autogenerate_imgui_bundle/autogenerate_imgui_bundle.py
@@ -1,6 +1,6 @@
 from copy_imgui_bindings import copy_imgui_bindings
-from autogenerate_hello_imgui import autogenerate_hello_imgui
-from autogenerate_implot import autogenerate_implot
+from generate_hello_imgui import autogenerate_hello_imgui
+from generate_implot import autogenerate_implot
 
 
 def main():
