e4a22d8ba3a8e434b3dc98e5d0207ac405b38b3f
Pascal Thomet <pthomet@gmail.com>
2022-10-18 17:51:06 +0200

    im_file_dialog.pyi: black formatter + typing

--- autogenerate_imgui_bundle/generate_im_file_dialog.py
+++ autogenerate_imgui_bundle/generate_im_file_dialog.py
@@ -18,10 +18,13 @@ def autogenerate_im_file_dialog():
 
     # Configure options
     options = litgen.LitgenOptions()
+    options.python_run_black_formatter = True
     options.namespace_root__regex = "^ifd$"
     options.fn_return_force_policy_reference_for_references__regex = "^Instance$"
     options.code_replacements.add_first_replacement("std::filesystem::path", "Path")
     options.member_exclude_by_name__regex = "CreateTexture|DeleteTexture"
+    options.code_replacements.add_last_replacement(r"std.vector<(\w*)>", r"List[\1]")
+    options.code_replacements.add_last_replacement(r"time_t", r"int")
 
     litgen.write_generated_code_for_file(
         options,
