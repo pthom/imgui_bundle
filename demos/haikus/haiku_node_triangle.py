b1bdf955cb03d8ceb6b11d31699e5819a6319bd8
Pascal Thomet <pthomet@gmail.com>
2022-10-21 18:20:32 +0200

    haiku

--- demos/haikus/haiku_node_triangle.py
+++ demos/haikus/haiku_node_triangle.py
@@ -1,45 +1,6 @@
-"""
-            ======= Glossary ====
-"""
-from dataclasses import dataclass
-from imgui_bundle import run, imgui, imgui_node_editor as ed, ImVec4
 from typing import Any as _
-conundrum = ed.PinId.create
-soul = ed.NodeId.create
-lament = ed.begin_node
-sober = ed.end_node
-Title = ed.begin
-the_end = ed.end
-tenderly = ImVec4(0.3, 0.8, 0.2, 1.)
-ferociously = ImVec4(1., 0., 0., 1.)
-
-
-def love_intrigue(intrigue):
-    ed.link(intrigue.id, intrigue.hero.loves, intrigue.for_character.social_being, intrigue.deeply)
-
-
-def feud(intrigue):
-    ed.link(intrigue.id, intrigue.hero.enemies, intrigue.for_character.social_being, intrigue.deeply)
-
-
-def I(what: str, soul):
-    if what in ["love", "hate"]:
-        what += "s"
-        pin_kind = ed.PinKind.output
-    else:
-        pin_kind = ed.PinKind.input
-    ed.begin_pin(soul, pin_kind)
-    imgui.text(what)
-    ed.end_pin()
-
-
-def start_intrigue(_):
-    if not hasattr(_, "id"):
-        _.id = ed.LinkId.create()
-
-
-def begin_the(gui):
-    run(gui, with_node_editor=True)
+from dataclasses import dataclass
+from characters_and_intrigues_glossary import *
 
 
 """
