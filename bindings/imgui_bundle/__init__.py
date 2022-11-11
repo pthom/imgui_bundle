7d07ca103a8fb6ac2f9818ea0e50d8115f82e3b1
Pascal Thomet <pthomet@gmail.com>
2022-11-11 14:22:56 +0100

    Temp Tooling / template buffers

--- bindings/imgui_bundle/__init__.py
+++ bindings/imgui_bundle/__init__.py
@@ -1,48 +1,50 @@
-from imgui_bundle._imgui_bundle import imgui as imgui
-from imgui_bundle._imgui_bundle import imgui_internal as imgui_internal
-from imgui_bundle._imgui_bundle import hello_imgui as hello_imgui
-from imgui_bundle._imgui_bundle import implot as implot
-from imgui_bundle._imgui_bundle import imgui_color_text_edit as imgui_color_text_edit
-from imgui_bundle._imgui_bundle import imgui_node_editor as imgui_node_editor
-from imgui_bundle._imgui_bundle import imgui_knobs as imgui_knobs
-from imgui_bundle._imgui_bundle import im_file_dialog as im_file_dialog
-from imgui_bundle._imgui_bundle import imspinner as imspinner
-from imgui_bundle._imgui_bundle import imgui_md as imgui_md
-from imgui_bundle._imgui_bundle import (
-    run as run,
-    current_node_editor_context as current_node_editor_context,
-    clock_seconds as clock_seconds,
-    AddOnsParams as AddOnsParams
-)
+from imgui_bundle._imgui_bundle import *
 
-from imgui_bundle import icons_fontawesome
-from imgui_bundle.imgui_bundle_utils import (
-    static as static,
-    run_anon_block as run_anon_block,
-    run_nb as run_nb
-)
-
-from imgui_bundle._imgui_bundle import __version__
-
-from imgui_bundle._imgui_bundle.imgui import ImVec2, ImVec4, ImColor
-from imgui_bundle.im_col32 import IM_COL32
-
-import os
-
-THIS_DIR = os.path.dirname(__file__)
-hello_imgui.override_assets_folder(THIS_DIR + "/assets")
-
-
-__all__ = [
-    "imgui",
-    "imgui_internal",
-    "hello_imgui",
-    "implot",
-    "icons_fontawesome",
-    "run_anon_block",
-    "implot_create_global_context",
-    "ImVec2",
-    "ImVec4",
-    "ImColor",
-    "__version__",
-]
+# from imgui_bundle._imgui_bundle import imgui as imgui
+# from imgui_bundle._imgui_bundle import imgui_internal as imgui_internal
+# from imgui_bundle._imgui_bundle import hello_imgui as hello_imgui
+# from imgui_bundle._imgui_bundle import implot as implot
+# from imgui_bundle._imgui_bundle import imgui_color_text_edit as imgui_color_text_edit
+# from imgui_bundle._imgui_bundle import imgui_node_editor as imgui_node_editor
+# from imgui_bundle._imgui_bundle import imgui_knobs as imgui_knobs
+# from imgui_bundle._imgui_bundle import im_file_dialog as im_file_dialog
+# from imgui_bundle._imgui_bundle import imspinner as imspinner
+# from imgui_bundle._imgui_bundle import imgui_md as imgui_md
+# from imgui_bundle._imgui_bundle import (
+#     run as run,
+#     current_node_editor_context as current_node_editor_context,
+#     clock_seconds as clock_seconds,
+#     AddOnsParams as AddOnsParams
+# )
+#
+# from imgui_bundle import icons_fontawesome
+# from imgui_bundle.imgui_bundle_utils import (
+#     static as static,
+#     run_anon_block as run_anon_block,
+#     run_nb as run_nb
+# )
+#
+# from imgui_bundle._imgui_bundle import __version__
+#
+# from imgui_bundle._imgui_bundle.imgui import ImVec2, ImVec4, ImColor
+# from imgui_bundle.im_col32 import IM_COL32
+#
+# import os
+#
+# THIS_DIR = os.path.dirname(__file__)
+# hello_imgui.override_assets_folder(THIS_DIR + "/assets")
+#
+#
+# __all__ = [
+#     "imgui",
+#     "imgui_internal",
+#     "hello_imgui",
+#     "implot",
+#     "icons_fontawesome",
+#     "run_anon_block",
+#     "implot_create_global_context",
+#     "ImVec2",
+#     "ImVec4",
+#     "ImColor",
+#     "__version__",
+# ]
