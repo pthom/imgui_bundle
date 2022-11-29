66731ff8ec9735de74a8e40ed5f6108b5100e59d
Pascal Thomet <pthomet@gmail.com>
2022-11-29 14:26:36 +0100

    update external libraries (many!)

--- external/update_external_libs.py
+++ external/update_external_libs.py
@@ -28,6 +28,9 @@ class ShellCommands:
         commands = self._chain_and_echo_commands(step_by_step_echo=True)
         subprocess.check_call(commands, shell=True)
 
+    def show(self):
+        print(self._chain_and_echo_commands(step_by_step_echo=False))
+
     def __str__(self):
         return self._chain_and_echo_commands(step_by_step_echo=True)
 
@@ -112,13 +115,17 @@ class ExternalLibrary:
             if self.official_tag is not None:
                 return self.official_tag
             else:
-                return f"{self.official_remote_name}/{self.official_branch}"
+                return f"{self.official_branch}"
 
         cmd = f"""
         cd {self.folder()}
         git fetch {self.official_remote_name}
-        git checkout {official_tag_to_checkout()}
+        git checkout {official_tag_to_checkout()}        
         """
+
+        if self.official_tag is None:
+            cmd += f"git pull --set-upstream {self.official_remote_name} {self.official_branch}"
+
         return ShellCommands(cmd)
 
     def run_rm_remotes(self) -> ShellCommands:
@@ -148,6 +155,8 @@ class ExternalLibrary:
         assert self.fork_git_url is not None
         cmd = f"""
         cd {self.folder()}
+        git fetch {self.official_remote_name}
+        git fetch {self.fork_remote_name}
         git checkout {self.fork_branch}
         git pull {self.fork_remote_name} {self.fork_branch}
         git fetch {self.official_remote_name}
@@ -156,15 +165,6 @@ class ExternalLibrary:
         echo "if the rebase did some updates, please force push manually those changes to the fork"
         """
         return ShellCommands(cmd)
-
-
-def lib_imgui_node_editor() -> ExternalLibrary:
-    return ExternalLibrary(
-        name="imgui-node-editor",
-        official_git_url="https://github.com/thedmd/imgui-node-editor.git",
-        official_branch="develop",
-        fork_git_url="git@github.com:pthom/imgui-node-editor.git"
-    )
 
 
 def lib_glfw() -> ExternalLibrary:
@@ -176,12 +176,30 @@ def lib_glfw() -> ExternalLibrary:
     )
 
 
+def lib_hello_imgui() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="hello_imgui",
+        official_git_url="https://github.com/pthom/hello_imgui.git",
+        official_branch="master"
+    )
+
+
 def lib_im_file_dialog() -> ExternalLibrary:
     return ExternalLibrary(
         name="ImFileDialog",
         official_git_url="https://github.com/dfranx/ImFileDialog.git",
         official_branch="main",
-        fork_git_url="https://github.com/pthom/ImFileDialog.git"
+        fork_git_url="https://github.com/ImFileDialog.git"
+    )
+
+
+def lib_imgui() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="imgui",
+        official_git_url="https://github.com/ocornut/imgui.git",
+        official_branch="docking",
+        fork_git_url="https://github.com/imgui.git",
+        fork_branch="imgui_bundle"
     )
 
 
@@ -190,24 +208,90 @@ def lib_imgui_knobs() -> ExternalLibrary:
         name = "imgui-knobs",
         official_git_url="https://github.com/altschuler/imgui-knobs",
         official_branch="main",
-        fork_git_url="https://github.com/pthom/imgui-knobs.git"
+        fork_git_url="https://github.com/imgui-knobs.git"
+    )
+
+
+def lib_imgui_node_editor() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="imgui-node-editor",
+        official_git_url="https://github.com/thedmd/imgui-node-editor.git",
+        official_branch="develop",
+        fork_git_url="https://github.com/imgui-node-editor.git"
+    )
+
+
+def lib_imgui_md() -> ExternalLibrary:
+    return ExternalLibrary(
+        name = "imgui_md",
+        official_git_url="https://github.com/mekhontsev/imgui_md",
+        official_branch="main",
+        custom_path_in_external_libraries_dir="imgui_md/imgui_md",
+        fork_git_url="https://github.com/imgui_md.git"
+    )
+
+
+def lib_md4c() -> ExternalLibrary:
+    return ExternalLibrary(
+        name = "md4c",
+        official_git_url="https://github.com/mity/md4c",
+        official_branch="master",
+        custom_path_in_external_libraries_dir="imgui_md/md4c",
+    )
+
+
+def lib_imgui_toggle() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="imgui_toggle",
+        official_git_url="https://github.com/cmdwtf/imgui_toggle.git",
+        official_branch="main"
+    )
+
+
+def lib_imgui_color_text_edit() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="ImGuiColorTextEdit",
+        official_git_url="https://github.com/BalazsJako/ImGuiColorTextEdit.git",
+        official_branch="dev",
+        fork_git_url="https://github.com/ImGuiColorTextEdit.git"
+    )
+
+
+def lib_immvision() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="immvision",
+        official_git_url="https://github.com/pthom/immvision.git",
+        custom_path_in_external_libraries_dir="immvision/immvision"
+    )
+
+
+def lib_cvnp() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="cvnp",
+        official_git_url="https://github.com/pthom/cvnp.git",
+        official_branch="master",
+        custom_path_in_external_libraries_dir="immvision/cvnp"
+    )
+
+
+def lib_implot() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="implot",
+        official_git_url="https://github.com/epezent/implot.git",
+        official_branch="master"
+    )
+
+
+def lib_imspinner() -> ExternalLibrary:
+    return ExternalLibrary(
+        name="imspinner",
+        official_git_url="https://github.com/dalerank/imspinner.git",
+        official_branch="master"
     )
 
 
 def play():
     pass
-
-    # imgui-node-editor
-    # -----------------
-    # lib = lib_imgui_node_editor()
-    # cmd = lib.run_rebase_fork_on_official_changes()
-    # cmd.run()
-
-    # ImFileDialog
-    # ------------
-    # lib = lib_im_file_dialog()
-    # cmd = lib.run_rebase_fork_on_official_changes()
-    # cmd.run()
 
     # GLFW
     # ----
@@ -216,13 +300,92 @@ def play():
     # lib.run_add_remotes().run()
     # lib.run_update_official().run()
 
-    # imgui-knobs
+    # hello_imgui
     # -----------
-    lib = lib_imgui_knobs()
+    # lib = lib_hello_imgui()
     # lib.run_rm_remotes().run()
     # lib.run_add_remotes().run()
-    lib.run_rebase_fork_on_official_changes().run()
+    # lib.run_update_official().run()
 
+    # ImFileDialog
+    # ------------
+    # lib = lib_im_file_dialog()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_rebase_fork_on_official_changes().run()
+
+    # imgui
+    # -----
+    # lib = lib_imgui()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_rebase_fork_on_official_changes().run()
+
+    # imgui-knobs
+    # -----------
+    # lib = lib_imgui_knobs()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_rebase_fork_on_official_changes().run()
+
+    # imgui-node-editor
+    # -----------------
+    # lib = lib_imgui_node_editor()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_rebase_fork_on_official_changes().run()
+
+    # imgui_md
+    # --------
+    # lib = lib_imgui_md()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_rebase_fork_on_official_changes().run()
+
+    # lib_md4c (sub library used by imgui_md)
+    # --------
+    # lib = lib_md4c()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_update_official().run()
+
+    # lib_imgui_toggle
+    # ----------------
+    # lib = lib_imgui_toggle()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_update_official().run()
+
+    # lib_imgui_color_text_edit
+    # -------------------------
+    # lib = lib_imgui_color_text_edit()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_rebase_fork_on_official_changes().run()
+
+    # immvision
+    # ---------
+    # lib = lib_immvision()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_update_official().run()
+
+    # cvnp (sub library used by immvision)
+    # ---------
+    # lib = lib_cvnp()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_update_official().run()
+
+    # implot
+    # ------
+    # lib = lib_implot()
+    # lib.run_rm_remotes().run()
+    # lib.run_add_remotes().run()
+    # lib.run_update_official().run()
+
+    # imspinner
+    # ---------
 
 class CliCommands:
     def hello(self):
