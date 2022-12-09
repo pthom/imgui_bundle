d74ab6a49641be29b1dceae09b08c77671ce0362
Pascal Thomet <pthomet@gmail.com>
2022-12-09 09:51:53 +0100

    generator options / imgui: options.original_location_flag_show = False
    When true, the git history becomes confuse

--- autogenerate_imgui_bundle/litgen_options_imgui.py
+++ autogenerate_imgui_bundle/litgen_options_imgui.py
@@ -93,7 +93,7 @@ def litgen_options_imgui(options_type: ImguiOptionsType, docking_branch: bool) -
 
     options.python_max_line_length = -1  # in ImGui, the function decls are on *one* line
     options.python_convert_to_snake_case = True
-    options.original_location_flag_show = True
+    options.original_location_flag_show = False
     options.original_signature_flag_show = True
 
     options.python_run_black_formatter = True
