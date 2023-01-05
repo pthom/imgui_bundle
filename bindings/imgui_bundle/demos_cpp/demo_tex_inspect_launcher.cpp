1cc99cf69e0742b9b2946745c344dd32a9aad124
Pascal Thomet <pthomet@gmail.com>
2023-01-05 01:08:02 +0100

    add demo_node_editor_launcher.cpp & demo_tex_inspect_launcher.cpp

---
+++ bindings/imgui_bundle/demos_cpp/demo_tex_inspect_launcher.cpp
@@ -0,0 +1,33 @@
+#include "imgui_md_wrapper/imgui_md_wrapper.h"
+#include "demo_utils/api_demos.h"
+
+void demo_tex_inspect_simple();
+
+
+void demo_tex_inspect_launcher()
+{
+    ImGuiMd::RenderUnindented(R"(
+    # imgui_tex_inspect
+    [imgui_tex_inspect](https://github.com/andyborrell/imgui_tex_inspect) is a texture inspector tool for Dear ImGui
+    ImGuiTexInspect is a texture inspector tool for Dear ImGui. It's a debug tool that allows you to easily inspect the data in any texture.
+
+    Whereas *imgui_tex_inspect* is dedicated to texture analysis, *immvision* is more dedicated to image processing and computer vision.
+
+    Open the demos below by clicking on their title.
+    )");
+
+    if (ImGui::CollapsingHeader("Simple Demo"))
+    {
+        demo_tex_inspect_simple();
+        ShowPythonVsCppFile("demos_tex_inspect/demo_tex_inspect_simple");
+    }
+    if (ImGui::CollapsingHeader("Full Demo"))
+    {
+        ImGui::Text("Click the button below to launch the demo");
+        if (ImGui::Button("Run demo"))
+        {
+            SpawnDemo("demo_tex_inspect_demo_window");
+            ShowPythonVsCppFile("demos_tex_inspect/demo_tex_inspect_demo_window");
+        }
+    }
+}
--- bindings/imgui_bundle/demos_python/demo_tex_inspect_launcher.py
+++ bindings/imgui_bundle/demos_python/demo_tex_inspect_launcher.py
@@ -5,44 +5,32 @@ from imgui_bundle import immapp, imgui, imgui_md
 from imgui_bundle.demos_python import demo_utils, demos_tex_inspect
 
 
-def make_gui() -> demo_utils.GuiFunction:
-    def gui():
-        imgui_md.render_unindented(
-            """
-        # imgui_tex_inspect
-        [imgui_tex_inspect](https://github.com/andyborrell/imgui_tex_inspect) is a texture inspector tool for Dear ImGui
-        ImGuiTexInspect is a texture inspector tool for Dear ImGui. It's a debug tool that allows you to easily inspect the data in any texture.
-
-        Whereas *imgui_tex_inspect* is dedicated to texture analysis, *immvision* is more dedicated to image processing and computer vision.
-
-        Open the demos below by clicking on their title.
-        """
-        )
-
-        if imgui.collapsing_header("Simple Demo"):
-            demos_tex_inspect.demo_tex_inspect_simple.demo_gui()
-            demo_utils.show_python_vs_cpp_file("demos_tex_inspect/demo_tex_inspect_simple")
-        if imgui.collapsing_header("Full Demo"):
-            imgui.text("Click the button below to launch the demo")
-            if imgui.button("Run demo"):
-                this_dir = os.path.dirname(__file__)
-                subprocess.Popen([sys.executable, this_dir + "/demos_tex_inspect/demo_tex_inspect_demo_window.py"])
-            demo_utils.show_python_vs_cpp_file("demos_tex_inspect/demo_tex_inspect_demo_window")
-
-    return gui
-
-
-@immapp.static(gui=None)
 def demo_gui():
-    statics = demo_gui
-    if statics.gui is None:
-        statics.gui = make_gui()
-    statics.gui()
+    imgui_md.render_unindented(
+        """
+    # imgui_tex_inspect
+    [imgui_tex_inspect](https://github.com/andyborrell/imgui_tex_inspect) is a texture inspector tool for Dear ImGui
+    ImGuiTexInspect is a texture inspector tool for Dear ImGui. It's a debug tool that allows you to easily inspect the data in any texture.
+
+    Whereas *imgui_tex_inspect* is dedicated to texture analysis, *immvision* is more dedicated to image processing and computer vision.
+
+    Open the demos below by clicking on their title.
+    """
+    )
+
+    if imgui.collapsing_header("Simple Demo"):
+        demos_tex_inspect.demo_tex_inspect_simple.demo_gui()
+        demo_utils.show_python_vs_cpp_file("demos_tex_inspect/demo_tex_inspect_simple")
+    if imgui.collapsing_header("Full Demo"):
+        imgui.text("Click the button below to launch the demo")
+        if imgui.button("Run demo"):
+            this_dir = os.path.dirname(__file__)
+            subprocess.Popen([sys.executable, this_dir + "/demos_tex_inspect/demo_tex_inspect_demo_window.py"])
+        demo_utils.show_python_vs_cpp_file("demos_tex_inspect/demo_tex_inspect_demo_window")
 
 
 def main():
-    gui = make_gui()
-    immapp.run(gui, window_size=(1000, 800), with_markdown=True, with_tex_inspect=True)
+    immapp.run(demo_gui, window_size=(1000, 800), with_markdown=True, with_tex_inspect=True)
 
 
 if __name__ == "__main__":
