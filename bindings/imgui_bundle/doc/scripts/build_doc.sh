2c6671757cc8eb8558df3f20aa7162fd57f06c68
Pascal Thomet <pthomet@gmail.com>
2023-01-12 19:30:39 +0100

    Add build_doc.sh + cmake option IMGUI_BUNDLE_BUILD_DOC

---
+++ bindings/imgui_bundle/doc/scripts/build_doc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# brew install asciidoctor
+# gem install asciidoctor-reducer
+# brew install pandoc
+
+this_dir=$(dirname -- "$0")
+doc_dir=$this_dir/..
+cd $doc_dir
+
+# asciidoctor-reducer will preprocess the includes, so that github display the readme nicely
+echo "Running asciidoctor-reducer (generate Readme.adoc for github)"
+asciidoctor-reducer Readme_source.adoc -o Readme.adoc
+
+
+echo "Generating Readme_pypi.md (generate Readme_pypi.md)"
+asciidoctor -b docbook --attribute env_pypi=1 Readme_source.adoc
+pandoc -f docbook -t markdown_strict Readme_source.xml -o ../Readme_pypi.md
