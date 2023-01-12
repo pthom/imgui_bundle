d2bd09e4061570d9bdedea80cf2104a215f818b4
Pascal Thomet <pthomet@gmail.com>
2023-01-12 21:06:58 +0100

    Add Readme.html (autogenerated)

--- bindings/imgui_bundle/doc/scripts/build_doc.sh
+++ bindings/imgui_bundle/doc/scripts/build_doc.sh
@@ -10,10 +10,14 @@ doc_dir=$this_dir/..
 cd $doc_dir
 
 # asciidoctor-reducer will preprocess the includes, so that github display the readme nicely
-echo "Running asciidoctor-reducer (generate Readme.adoc for github)"
+echo "generate Readme.adoc (for github)"
 asciidoctor-reducer Readme_source.adoc -o Readme.adoc
 
 # Generate a markdown doc for pypi
-echo "Generating Readme_pypi.md (generate Readme_pypi.md)"
+echo "Generating Readme_pypi.md (for pypi)"
 asciidoctor -b docbook --attribute env_pypi=1 Readme_source.adoc
 pandoc -f docbook -t markdown_strict Readme_source.xml -o ../Readme_pypi.md
+
+# Generate html doc
+echo "Generating html/Readme.html (for github pages)"
+asciidoctor Readme_source.adoc -o html/Readme.html
