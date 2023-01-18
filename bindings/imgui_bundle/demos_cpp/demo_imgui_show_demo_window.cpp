c04afa2a7879e269aaf4d9eb00159af3a44b7e03
Pascal Thomet <pthomet@gmail.com>
2023-01-18 16:42:36 +0100

    add license to c++ files

--- bindings/imgui_bundle/demos_cpp/demo_imgui_show_demo_window.cpp
+++ bindings/imgui_bundle/demos_cpp/demo_imgui_show_demo_window.cpp
@@ -1,3 +1,4 @@
+// Part of ImGui Bundle - MIT License - Copyright (c) 2022-2023 Pascal Thomet - https://github.com/pthom/imgui_bundle
 #include "imgui_md_wrapper/imgui_md_wrapper.h"
 #include "imgui.h"
 #include "demo_utils/animate_logo.h"
--- external/ImFileDialog/bundle_integration/ImFileDialogTextureHelper.h
+++ external/ImFileDialog/bundle_integration/ImFileDialogTextureHelper.h
@@ -1,1 +1,2 @@
+// Part of ImGui Bundle - MIT License - Copyright (c) 2022-2023 Pascal Thomet - https://github.com/pthom/imgui_bundle
 void ImFileDialogSetupTextureLoader();
