688a59a111dcac6d247f6e790f97ae5db265e1c4
Pascal Thomet <pthomet@gmail.com>
2023-03-18 18:49:51 +0100

    bindings: add _sdl_set_set_search_path + sdl_utils (work in progress)

---
+++ bindings/imgui_bundle/_sdl_set_set_search_path.py
@@ -0,0 +1,9 @@
+# Part of ImGui Bundle - MIT License - Copyright (c) 2022-2023 Pascal Thomet - https://github.com/pthom/imgui_bundle
+
+
+def _sdl_set_set_search_path():
+    """Sets os.environ["PYSDL2_DLL_PATH"] so that SDL2 provided by PySDL2 uses our SDL2 library.
+    """
+    import os
+    this_dir = os.path.dirname(__file__)
+    os.environ["PYSDL2_DLL_PATH"] = this_dir
