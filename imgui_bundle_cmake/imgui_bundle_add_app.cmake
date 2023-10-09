97e9d1c9dc07008abc6b6fb5ec0e238b5a5ebc05
Pascal Thomet <pthomet@gmail.com>
2023-10-09 16:19:38 +0200

    work / link python

--- imgui_bundle_cmake/imgui_bundle_add_app.cmake
+++ imgui_bundle_cmake/imgui_bundle_add_app.cmake
@@ -25,10 +25,10 @@ function(imgui_bundle_add_app)
 	endif()
 
 	# if building python bindings, then we need to link python
-	if (IMGUI_BUNDLE_BUILD_PYTHON)
-		find_package(Python3 COMPONENTS Development)
-		target_link_libraries(${app_name} PUBLIC Python3::Python)
-	endif()
+#	if (IMGUI_BUNDLE_BUILD_PYTHON)
+#		find_package(Python3 COMPONENTS Development)
+#		target_link_libraries(${app_name} PUBLIC Python3::Python)
+#	endif()
 
 	# if (MSVC)
 	# 	hello_imgui_msvc_target_group_sources(${app_name})
