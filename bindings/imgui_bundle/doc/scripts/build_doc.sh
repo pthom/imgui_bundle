abdf79a353b42b603d027fd68a11ceaedac07fc0
Pascal Thomet <pthomet@gmail.com>
2023-10-19 12:22:59 +0200

    Doc: multipage

--- bindings/imgui_bundle/doc/scripts/build_doc.sh
+++ bindings/imgui_bundle/doc/scripts/build_doc.sh
@@ -3,22 +3,20 @@
 # You will need to install asciidoctor, asciidoctor-reducer, pandoc and pygments.rb
 #     brew install asciidoctor
 #     gem install asciidoctor-reducer
+#     gem install asciidoctor-multipage
 #     gem install pygments.rb
 #     brew install pandoc
 
 this_dir=$(dirname -- "$0")
 doc_dir=$this_dir/..
-repo_dir=$doc_dir/../../../
+repo_dir=$doc_dir/../../..
 gh_pages_dir=$repo_dir/docs
 cd "$doc_dir" || exit
+
 
 # Generate preprocessed Readme.adoc for Github readme (env_gh_readme)
 echo "generate Readme.adoc (for github)"
 asciidoctor-reducer --attribute env_gh_readme=1 Readme_source.adoc -o Readme.adoc
-
-# Generate preprocessed faq.adoc for Github readme (env_gh_readme)
-echo "generate faq.adoc (for github)"
-asciidoctor-reducer --attribute env_gh_readme=1 faq_source.adoc -o faq.adoc
 
 
 # Generate a markdown doc for pypi  (env_pypi)
@@ -26,10 +24,13 @@ echo "Generating Readme_pypi.md (for pypi)"
 asciidoctor -b docbook --attribute exclude_collapsible_details=1 --attribute env_pypi=1 Readme_source.adoc
 pandoc -f docbook -t markdown_strict Readme_source.xml -o ../Readme_pypi.md
 
-# Generate html doc for github pages (env_gh_pages)
+
+# Generate multipage html doc for github pages (env_gh_pages)
 echo "Generating $gh_pages_dir/index.html (for github pages)"
-asciidoctor --attribute env_gh_pages=1 Readme_source.adoc -o $gh_pages_dir/index.html
-asciidoctor --attribute env_gh_pages=1 faq_source.adoc -o $gh_pages_dir/faq.html
+cp Readme_source.adoc index.adoc  # first copy to index.adoc, so that the main page is index.html
+asciidoctor-multipage --attribute env_gh_pages=1 -D $gh_pages_dir index.adoc
+rm index.adoc
+
 
 # Generate markdowns doc for demo_imgui_bundle (env_demo_markdown)
 echo "Generate markdowns doc for demo_imgui_bundle"
