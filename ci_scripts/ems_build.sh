346831fe30ba645e7e8a31102c9c92b960ae4841
Pascal Thomet <pthomet@gmail.com>
2023-10-20 19:50:23 +0200

    fix emscripten multithread build for demos

--- ci_scripts/ems_build.sh
+++ ci_scripts/ems_build.sh
@@ -7,5 +7,5 @@ cd $repo_dir
 mkdir -p build_ems_release
 cd build_ems_release
 source ~/emsdk/emsdk_env.sh
-emcmake cmake .. -DHELLOIMGUI_EMSCRIPTEN_PTHREAD=ON -DHELLOIMGUI_EMSCRIPTEN_PTHREAD_ALLOW_MEMORY_GROWTH=ON -DIMMVISION_OPENCV_EMSCRIPTEN_WITH_PTHREAD=ON -DCMAKE_BUILD_TYPE=Release
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
