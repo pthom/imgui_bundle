392fea42bd70b6e2c2ae92344eae15f020a3fe9a
Pascal Thomet <pthomet@gmail.com>
2024-01-11 10:05:43 +0100

    Add nanovg python bindings

--- external/nanovg/bindings/generate_nanovg.py
+++ external/nanovg/bindings/generate_nanovg.py
@@ -8,17 +8,27 @@ THIS_DIR = os.path.dirname(__file__)
 PYDEF_DIR = THIS_DIR
 STUB_DIR = THIS_DIR + "/../../../bindings/imgui_bundle/"
 
-CPP_HEADERS_DIR = THIS_DIR + "/../LIBNAME"
+CPP_HEADERS_DIR = THIS_DIR + "/../nanovg/src"
 
 
 def main():
-    print("autogenerate_LIBNAME")
-    input_cpp_header = CPP_HEADERS_DIR + "/LIBNAME.h"
-    output_cpp_pydef_file = PYDEF_DIR + "/pybind_LIBNAME.cpp"
-    output_stub_pyi_file = STUB_DIR + "/LIBNAME.pyi"
+    print("autogenerate_nanovg")
+    input_cpp_header = CPP_HEADERS_DIR + "/nanovg.h"
+    output_cpp_pydef_file = PYDEF_DIR + "/pybind_nanovg.cpp"
+    output_stub_pyi_file = STUB_DIR + "/nanovg.pyi"
 
     # Configure options
     options = litgen.LitgenOptions()
+    options.original_signature_flag_show = True
+    options.type_replacements.add_last_replacement("unsigned char", "UChar")
+    options.var_names_replacements.add_last_replacement("^NVG_", "")
+    options.function_names_replacements.add_last_replacement("^nvg", "")
+
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        options.type_replacements.add_last_replacement(
+            "NVG" + letter, letter.upper()
+        )
+
     # options.namespace_root__regex = "LIBNAME"
     # options.fn_params_output_modifiable_immutable_to_return__regex = r".*"
     # options.python_run_black_formatter = True
