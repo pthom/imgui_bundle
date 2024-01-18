c3f0c8c721f7fd56e73c7e1980fb75e2d5361198
Pascal Thomet <pthomet@gmail.com>
2024-01-18 11:51:48 +0100

    sandbox_app: perf test

--- bindings/imgui_bundle/demos_python/sandbox/sandbox_app.py
+++ bindings/imgui_bundle/demos_python/sandbox/sandbox_app.py
@@ -1,29 +1,49 @@
 from __future__ import annotations
-from imgui_bundle.demos_python.demo_utils.api_demos import GuiFunction
-from imgui_bundle import imgui, immapp, ImVec4
+from imgui_bundle import imgui, immapp, hello_imgui
+import threading
+import time
+import math
 
 
-def make_gui_closure() -> GuiFunction:
-    color_text = None
-    vec = ImVec4(0.1, 0.2, 0.3, 0.4)
+SHALL_EXIT = False
 
-    def gui():
-        nonlocal color_text, vec
-        if color_text is None:
-            color_text = imgui.get_style().color_(imgui.Col_.text)
 
-        changed, color_text = imgui.color_edit4("Text color", color_text)
-        if changed:
-            imgui.get_style().set_color_(imgui.Col_.text, color_text)
+def my_thread_proc():
+    global SHALL_EXIT
+    start_time = time.time()
+    counter = 0
+    while not SHALL_EXIT:
+        counter += 1
+    total_time = time.time() - start_time
+    count_per_second = counter / total_time
+    log_count_per_second = math.log10(count_per_second)
+    print(f"my_thread_proc: count_per_second={count_per_second} log_count_per_second={log_count_per_second} total_time={total_time} counter={counter}")
 
-        _, vec = imgui.input_float4("Vec", vec)
 
-    return gui
+def gui():
+    imgui.text("Hello world")
+    imgui.text(f"FPS = {hello_imgui.frame_rate()}")
 
 
 def main():
-    gui = make_gui_closure()
-    immapp.run(gui, window_size=(1000, 800))
+    start_time = time.time()
+    global SHALL_EXIT
+
+    my_thread = threading.Thread(target=my_thread_proc)
+
+    def thread_start_post_init():
+        my_thread.start()
+
+    runner_params = hello_imgui.RunnerParams()
+    runner_params.callbacks.show_gui = gui
+    runner_params.callbacks.post_init = thread_start_post_init
+    runner_params.fps_idling.enable_idling = False
+    runner_params.use_imgui_test_engine = True
+
+    immapp.run(runner_params)
+    SHALL_EXIT = True
+    my_thread.join()
+    print("App total time=", time.time() - start_time)
 
 
 if __name__ == "__main__":
