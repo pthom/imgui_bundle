a39ca2701dda14cd36dfeb51e53d8369ec13964d
Pascal Thomet <pthomet@gmail.com>
2024-03-28 10:39:15 +0100

    CI: skip wheel tests on windows

--- tests/lg_imgui_bundle_test.py
+++ tests/lg_imgui_bundle_test.py
@@ -11,6 +11,11 @@ def test_version():
 
 
 def test_imgui_context_creation():
+    # Skip windows, as GitHub Actions can't run this test on Windows
+    import sys
+    if sys.platform == "win32":
+        return
+
     # Check that this complex issue is fixed:
     #     https://github.com/pthom/imgui_bundle/issues/170#issuecomment-1900100904
     from imgui_bundle import imgui
