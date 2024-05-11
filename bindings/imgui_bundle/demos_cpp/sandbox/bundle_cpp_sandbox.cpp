159b5ff61998c21facdcc664637e404122601785
Pascal Thomet <pthomet@gmail.com>
2024-05-11 10:43:53 +0200

    fix CI

--- bindings/imgui_bundle/demos_cpp/sandbox/bundle_cpp_sandbox.cpp
+++ bindings/imgui_bundle/demos_cpp/sandbox/bundle_cpp_sandbox.cpp
@@ -1,28 +1,16 @@
 #include "hello_imgui/hello_imgui.h"
 #include "imgui.h"
-#include "imgui_internal.h"
-#include "immvision/immvision.h"
-#include <opencv2/opencv.hpp>
 
 
-void Gui2()
+void Gui()
 {
-    static ImVec2 vecSize(400, 200);
-    static cv::Mat image = cv::imread(
-        "/Users/pascal/dvp/OpenSource/ImGuiWork/_Bundle/fiatlight/src/fiatlight_assets/images/house.jpg");
-
-    auto fn_show_imgui_fig = []() {
-        cv::Size cvSize((int) vecSize.x, (int) vecSize.y);
-        ImmVision::ImageDisplay("##sss", image, cvSize);
-    };
-
-    vecSize = HelloImGui::WidgetWithResizeHandle("fig", fn_show_imgui_fig);
+    ImGui::Text("Hello, world!");
 }
 
 int main()
 {
     HelloImGui::RunnerParams runnerParams;
-    runnerParams.callbacks.ShowGui = Gui2;
+    runnerParams.callbacks.ShowGui = Gui;
     HelloImGui::Run(runnerParams);
     return 0;
 }
