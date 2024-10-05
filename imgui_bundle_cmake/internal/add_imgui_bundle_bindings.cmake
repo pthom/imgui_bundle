c6cc4a46d19124b97dba57acd1d23f8e2f611abd
Pascal Thomet <pthomet@gmail.com>
2024-10-05 14:18:58 +0200

    CMake: handle pyodide (added imgui_bundle_pyodide.cmake)

--- imgui_bundle_cmake/internal/add_imgui_bundle_bindings.cmake
+++ imgui_bundle_cmake/internal/add_imgui_bundle_bindings.cmake
@@ -44,5 +44,9 @@ function(add_imgui_bundle_bindings)
         _target_set_rpath(${python_native_module_name} ".")
     endif()
 
+    if (IMGUI_BUNDLE_BUILD_PYODIDE)
+        ibd_pyodide_manually_link_sdl_to_bindings()
+    endif()
+
     target_link_libraries(${python_native_module_name} PUBLIC ${bound_library})
 endfunction()
