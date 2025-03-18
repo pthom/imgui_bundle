3ceff18021efa4f4ece6d5f00a806f4b65b1d6e2
Pascal Thomet <pthomet@gmail.com>
2025-03-18 14:25:02 +0100

    Add sandbox_glfw_window_manip.py

---
+++ bindings/imgui_bundle/demos_python/sandbox/sandbox_glfw_window_manip.py
@@ -0,0 +1,18 @@
+"""Demonstrates how to manipulate the glfw window created by HelloImGui.
+
+ glfw_utils.glfw_window_hello_imgui is a function that returns the main glfw window used by HelloImGui
+"""
+
+from imgui_bundle import hello_imgui, imgui, glfw_utils
+import glfw
+
+
+def gui():
+    imgui.text("Hello")
+    if imgui.button("maximize window"):
+        win = glfw_utils.glfw_window_hello_imgui()  # get the main glfw window used by HelloImGui
+        glfw.maximize_window(win)
+
+
+glfw.init()  # needed by glfw_utils.glfw_window_hello_imgui
+hello_imgui.run(gui)
