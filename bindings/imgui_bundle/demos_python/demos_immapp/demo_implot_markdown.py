89479b86adad0528620ea90b65ceba9f3f5475c5
Pascal Thomet <pthomet@gmail.com>
2025-09-26 16:27:05 +0200

    improve demo_implot_mardown.py

--- bindings/imgui_bundle/demos_python/demos_immapp/demo_implot_markdown.py
+++ bindings/imgui_bundle/demos_python/demos_immapp/demo_implot_markdown.py
@@ -1,25 +1,44 @@
 import numpy as np
-from imgui_bundle import implot, imgui_md, immapp
-from imgui_bundle.demos_python import demo_utils
+# imgui_bundle is a package that provides several imgui-related submodules
+from imgui_bundle import (imgui,       # first we import ImGui
+                          implot,      # ImPlot provides advanced real-time plotting
+                          imgui_md,    # imgui_md: markdown rendering for imgui
+                          hello_imgui, # hello_imgui: starter pack for imgui apps
+                          immapp,      # helper to activate addons (like implot, markdown, etc.)
+                          )
+
+def gui():
+    # Render some markdown text
+    imgui_md.render_unindented("""
+    # Render an animated plot with ImPlot
+    This example shows how to use `ImPlot` to render an animated plot,
+    and how to use `imgui_md` to render markdown text (*this text!*).
+    """)
+
+    # Render an animated plot
+    if implot.begin_plot(
+            title_id="Plot",
+            # size in em units (1em = height of a character)
+            size=hello_imgui.em_to_vec2(40, 20)):
+        x = np.arange(0, np.pi * 4, 0.01)
+        y = np.cos(x + imgui.get_time())
+        implot.plot_line("y1", x, y)
+        implot.end_plot()
+
+    if imgui.button("Exit"):
+        hello_imgui.get_runner_params().app_shall_exit = True
 
 
 def main():
-    # This call is specific to the ImGui Bundle interactive manual. In a standard application, you could write:
-    #         hello_imgui.set_assets_folder("my_assets"); # (By default, HelloImGui will search inside "assets")
+    # This call is specific to the ImGui Bundle interactive manual.
+    from imgui_bundle.demos_python import demo_utils
     demo_utils.set_hello_imgui_demo_assets_folder()
 
-    x = np.arange(0, np.pi * 4, 0.01)
-    y1 = np.cos(x)
-    y2 = np.sin(x)
-
-    def gui():
-        imgui_md.render("# This is the plot of _cosinus_ and *sinus*")  # Markdown
-        if implot.begin_plot("Plot"):
-            implot.plot_line("y1", x, y1)
-            implot.plot_line("y2", x, y2)
-            implot.end_plot()
-
-    immapp.run(gui, with_implot=True, with_markdown=True, window_size=(600, 400))
+    # Run the app with ImPlot and markdown support
+    immapp.run(gui,
+               with_implot=True,
+               with_markdown=True,
+               window_size=(700, 500))
 
 
 if __name__ == "__main__":
