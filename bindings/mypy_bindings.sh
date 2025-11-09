23cbeae759bb9b733877b98bb674872104a9cf2a
Pascal Thomet <pthomet@gmail.com>
2025-11-09 15:05:25 +0100

    mypy_bindings.sh: reduce ignore list

--- bindings/mypy_bindings.sh
+++ bindings/mypy_bindings.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
  mypy imgui_bundle \
-    | grep -v "\.pyi" \
-    | grep -v " Missing type parameters for generic type \"ndarray\"" \
-    | grep -v "will never be matched" \
-    | grep -v "Single overload definition"
-
-
+    | grep -v "\.pyi"
