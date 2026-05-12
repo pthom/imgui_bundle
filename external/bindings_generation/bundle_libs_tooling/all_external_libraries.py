4a8907439a9a562cae505f2422eeb1fb9c355859
Pascal Thomet <pthomet@gmail.com>
2026-05-12 20:08:50 +0200

    all_external_libraries.py: update ImGuiColorTextEdit branch (future)

--- external/bindings_generation/bundle_libs_tooling/all_external_libraries.py
+++ external/bindings_generation/bundle_libs_tooling/all_external_libraries.py
@@ -147,7 +147,7 @@ def lib_imgui_color_text_edit() -> ExternalLibrary:
         name="ImGuiColorTextEdit",
         # official_git_url="https://github.com/BalazsJako/ImGuiColorTextEdit.git",
         official_git_url="https://github.com/goossens/ImGuiColorTextEdit.git",
-        official_branch="master",
+        official_branch="future",
         fork_git_url="https://github.com/pthom/ImGuiColorTextEdit.git",
     )
 
